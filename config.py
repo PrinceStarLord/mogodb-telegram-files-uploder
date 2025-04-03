575b2c8d63eaaec68fc1e96402c69cb217292a08
Tushar Padir <86295965+PrinceStarLord@users.noreply.github.com>
2025-04-03 19:32:39 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+#Replace all values with your values.
+
+API_ID = 276459
+API_HASH = "c7a57c7ad832f686b0359feb5"
+BOT_TOKEN = "6850677267:AAGkNuwXrDHMHl7rw-Q0xL1MY"
+
+ADMINS = [123456789]  
+TARGET_CHAT_ID = -10022060492  # Chat ID where files will be uploaded
+
+DATABASE_URI = "mongodb+srv://kshit:FQh@cluster0.kaohupy.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = "Cluster0"
+COLLECTION_NAME = "MoviesBot"
